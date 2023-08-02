fb9439baaea7a83a2069b484b9186e12da091db5
Tobias <tobiasdunn.02@gmail.com>
2023-08-02 15:48:33 +0100

    Add User APIs

--- server/common/date.ts
+++ server/common/date.ts
@@ -8,4 +8,12 @@ export const date_string_to_date = (date_string: string) => {
   return new Date(`${year}-${month}-${day}T00:00:00Z`);
 };
 
+export const get_today_date_as_string = (): string => {
+  const today = new Date();
+  const date_string = `${String(today.getDate()).padStart(2, "0")}-${String(
+    today.getUTCMonth() + 1
+  ).padStart(2, "0")}-${today.getUTCFullYear()}`;
+  return date_string;
+};
+
 export const MISSING_DATE_MSG = "Missing date in request";
---
+++ server/routes/user.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import {
+  add_email,
+  create_account,
+  get_email,
+  is_account_admin,
+  login,
+  update_email,
+  update_password,
+} from "../controller/userController";
+
+export const userRouter: Router = Router();
+
+// Create a new account
+userRouter.post("/signup", create_account);
+
+// Change a password for an account
+userRouter.patch("/change-password", update_password);
+
+// Login to an account
+userRouter.post("/login", login);
+
+// Check if an account is an administrator
+userRouter.get("/admin", is_account_admin);
+
+// Get an email for an account
+userRouter.get("/email", get_email);
+
+// Update an email for an account
+userRouter.patch("/email", update_email);
+
+// Add an email to an account
+userRouter.post("/email", add_email);
