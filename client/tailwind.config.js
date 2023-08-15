0c1bc00fafc1987504106b30dd2ce8bfa7288658
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2023-08-15 19:39:15 +0100

    Remove responsiveness of nav bar and switch to tailwind for css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        60: "60deg",
+      },
+    },
   },
   plugins: [],
 };
