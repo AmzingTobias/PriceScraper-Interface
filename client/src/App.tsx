7267c2e525b4b276de6759378c73027f459248de
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2023-08-28 10:44:38 +0100

    Create page for changing a product's information

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import ImageUploadPage from "./pages/image/image-upload";
 import NewProductPage from "./pages/product/new-product";
 import PriceScraperLogPage from "./pages/admin/price-scraper-log";
 import ManageImagesPage from "./pages/image/maange-images";
+import ManageProductPage from "./pages/product/manage-product";
 
 function validateAuthToken(authToken: string) {
   try {
@@ -132,6 +133,10 @@ export default function App() {
           <Route
             path="/admin/products/new"
             Component={() => NewProductPage({ userIsAdmin: userIsAdmin })}
+          />
+          <Route
+            path="/admin/products/:productId"
+            Component={() => ManageProductPage({ userIsAdmin: userIsAdmin })}
           />
           <Route
             path="/admin/scraper-log"
