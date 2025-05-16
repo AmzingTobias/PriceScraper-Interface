3ab2469efb557d2ead43a6118f01176b22e7e51a
Tobias <tobiasdunn.02@gmail.com>
2025-05-16 20:02:41 +0100

    Fix Date error

--- server/common/date.ts
+++ server/common/date.ts
@@ -1,11 +1,10 @@
 /**
  * Convert the date string in the format dd-mm-yyyy to a date object
- * @param date_string A string in the format of a date
+ * @param date Seconds since epoch time
  * @returns The date object extracted from teh string
  */
-export const date_string_to_date = (date_string: string) => {
-  const [day, month, year] = date_string.split("-");
-  return new Date(`${year}-${month}-${day}T00:00:00Z`);
+export const date_string_to_date = (date: number) => {
+  return new Date(date);
 };
 
 export const get_today_date_as_string = (): string => {
